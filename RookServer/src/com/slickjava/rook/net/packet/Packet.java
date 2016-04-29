c6a1f28d3b890b549f806f5af8d634d1c95fcbe0
SlickJava <slickjava@gmail.com>
2016-04-29 23:54:42 +1200

    Packet Update.

--- RookServer/src/com/slickjava/rook/net/packet/Packet.java
+++ RookServer/src/com/slickjava/rook/net/packet/Packet.java
@@ -3,13 +3,23 @@ package com.slickjava.rook.net.packet;
 public class Packet {
 
 	private PacketType type;
+	private byte[] data;
 	
-	public Packet(PacketType type)
+	public Packet(PacketType type, byte[] data)
 	{
 		this.type = type;
+		this.data = data;
 	}
 	
-	
+	public byte[] getData() {
+		return data;
+	}
+
+
+	public void setData(byte[] data) {
+		this.data = data;
+	}
+
 
 	public PacketType getType() {
 		return type;
