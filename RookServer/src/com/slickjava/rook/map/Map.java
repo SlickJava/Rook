449234260cda4652df1c66cb7a074a4e4f3b91d4
SlickJava <slickjava@gmail.com>
2016-04-30 21:41:37 +1200

    Added getTileByCoord(int x, int y) to Map.java.

--- RookServer/src/com/slickjava/rook/map/Map.java
+++ RookServer/src/com/slickjava/rook/map/Map.java
@@ -1,7 +1,59 @@
 package com.slickjava.rook.map;
 
+import com.slickjava.rook.map.tile.Tile;
+
 public class Map {
 	
+	private String name;
+	private Generation mapGen;
+	public int maxX;
+	public int maxY;
+	public Tile[][] map;
 	
+	public Map(String name, int maxX, int maxY)
+	{
+		mapGen = new Generation();
+		this.name = name;
+		this.maxX = maxX;
+		this.maxY = maxY;
+		this.map = mapGen.generateMap(maxX, maxY);
+		
+	}
+	
+	public Tile getTileByCoord(int x, int y)
+	{
+		Tile returnTile = null;
+		
+		for(int i = 0; i < maxY; i++)
+		{
+			for(int j = 0; j < maxX; j++)
+			{
+				if(i == y && j == x)
+					returnTile = this.map[i][j];
+			}
+		}
+		
+		if(returnTile == null)
+			System.out.println("getTileByCoord(" + x + ", " + y + ") is invalid.");
+		
+		return returnTile;
+	}
+	
+	public String getName() {
+		return name;
+	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Tile[][] getMap() {
+		return map;
+	}
+
+	public void setMap(Tile[][] map) {
+		this.map = map;
+	}
+
+	
 }
