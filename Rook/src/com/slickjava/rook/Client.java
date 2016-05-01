84c14ca4bc2b967e5e2345371bcd461442347be7
SlickJava <slickjava@gmail.com>
2016-05-01 13:54:50 +1200

    Packet system and encryption updates.

--- Rook/src/com/slickjava/rook/Client.java
+++ Rook/src/com/slickjava/rook/Client.java
@@ -1,7 +1,6 @@
 package com.slickjava.rook;
 
 import java.io.IOException;
-import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.security.InvalidKeyException;
@@ -12,6 +11,7 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 
 import com.slickjava.rook.net.packet.packets.N00Login;
+import com.slickjava.rook.net.packet.packets.N01Disconnect;
 import com.slickjava.rook.security.Encrypt;
 
 public class Client {
@@ -24,10 +24,16 @@ public class Client {
 		
         Encrypt encrypt = new Encrypt();
       
-        
-        N00Login login = new N00Login("SlickJava", "doge");
-        login.sendData(socket, address, 4444, encrypt);
-        
+        int counter = 0;
+        while(true)
+        {
+        	counter++;
+            N00Login login = new N00Login("SlickJava" + counter, "doge");
+            login.sendData(socket, address, 4444, encrypt);
+            N01Disconnect disconnect = new N01Disconnect("SlickJava", "doge");
+            disconnect.sendData(socket, address, 4444, encrypt);
+        }
+
 	}
 	
 }
